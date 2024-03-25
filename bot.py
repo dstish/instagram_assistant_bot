905a38c08838f865c42b4994282216e1d7e90fe5
daniil <stish.ads@gmai.com>
2024-03-25 23:58:30 +0300

    Add username

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import telebot
+from telebot.formatting import mbold, format_text, hbold
 
 bot_token = getenv("BOT_TOKEN")
 bot = telebot.TeleBot(bot_token)
@@ -25,14 +26,23 @@ def get_page_info(url):
         driver.get(url)
 
         # Wait until element by CSS selector becomes available
-        element = WebDriverWait(driver, 10).until(
+        element_username = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "a.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1q0g3np.x1lku1pv.x1a2a7pz.x6s0dn4.xjyslct.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x9f619.x1ypdohk.x1f6kntn.xwhw2v2.xl56j7k.x17ydfre.x2b8uid.xlyipyv.x87ps6o.x14atkfc.xcdnw81.x1i0vuye.xjbqb8w.xm3z3ea.x1x8b98j.x131883w.x16mih1h.x972fbf.xcfux6l.x1qhh985.xm0m39n.xt0psk2.xt7dq6l.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x1n5bzlp.xqnirrm.xj34u2y.x568u83"))
+        )
+
+        # Get username from the element
+        username = element_username.text
+
+        element_description = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "div._a9zs"))
         )
 
         # Get text from the element
-        description = element.text
+        description = f"{format_text(hbold(username))} {element_description.text}"
 
-        # Try to find an image in div._aagv
+
+
+        # Try to find image
         try:
             element_media = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located(
@@ -76,16 +86,16 @@ def handle_message(message):
         if media_url and "mp4" in media_url:
             if len(description) <= 1024:
                 # If the link leads to a video, send the video
-                bot.send_video(message.chat.id, media_url, caption=description)
+                bot.send_video(message.chat.id, media_url, caption=description, parse_mode="HTML")
             else:
-                bot.send_video(message.chat.id, media_url, caption=description[:1024])
+                bot.send_video(message.chat.id, media_url, caption=description[:1024], parse_mode="markdown")
                 bot.send_message(message.chat.id, description[1024:])
         else:
             # Otherwise, send the image
             if len(description) <= 1024:
-                bot.send_photo(message.chat.id, media_url, caption=description)
+                bot.send_photo(message.chat.id, media_url, caption=description, parse_mode="HTML")
             else:
-                bot.send_photo(message.chat.id, media_url, caption=description[:1024])
+                bot.send_photo(message.chat.id, media_url, caption=description[:1024], parse_mode="markdown")
                 bot.send_message(message.chat.id, description[1024:])
 
     except Exception as e:
