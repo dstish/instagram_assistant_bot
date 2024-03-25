298902cd5aa0381c20573f07519d096fad9058c2
daniil <stish.ads@gmai.com>
2024-03-26 00:00:54 +0300

    Add username (some changes)

--- bot.py
+++ bot.py
@@ -27,7 +27,16 @@ def get_page_info(url):
 
         # Wait until element by CSS selector becomes available
         element_username = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "a.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1q0g3np.x1lku1pv.x1a2a7pz.x6s0dn4.xjyslct.x1ejq31n.xd10rxx.x1sy0etr.x17r0tee.x9f619.x1ypdohk.x1f6kntn.xwhw2v2.xl56j7k.x17ydfre.x2b8uid.xlyipyv.x87ps6o.x14atkfc.xcdnw81.x1i0vuye.xjbqb8w.xm3z3ea.x1x8b98j.x131883w.x16mih1h.x972fbf.xcfux6l.x1qhh985.xm0m39n.xt0psk2.xt7dq6l.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x1n5bzlp.xqnirrm.xj34u2y.x568u83"))
+            EC.presence_of_element_located((By.CSS_SELECTOR, "a.x1i10hfl.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.xdl72j9"
+                                                             ".x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r"
+                                                             ".x2lwn1j.xeuugli.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt"
+                                                             ".x1q0g3np.x1lku1pv.x1a2a7pz.x6s0dn4.xjyslct.x1ejq31n"
+                                                             ".xd10rxx.x1sy0etr.x17r0tee.x9f619.x1ypdohk.x1f6kntn"
+                                                             ".xwhw2v2.xl56j7k.x17ydfre.x2b8uid.xlyipyv.x87ps6o"
+                                                             ".x14atkfc.xcdnw81.x1i0vuye.xjbqb8w.xm3z3ea.x1x8b98j"
+                                                             ".x131883w.x16mih1h.x972fbf.xcfux6l.x1qhh985.xm0m39n"
+                                                             ".xt0psk2.xt7dq6l.xexx8yu.x4uap5.x18d9i69.xkhd6sd"
+                                                             ".x1n2onr6.x1n5bzlp.xqnirrm.xj34u2y.x568u83"))
         )
 
         # Get username from the element
